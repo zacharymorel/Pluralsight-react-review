f926a02ef27095fb5f029ab7f3a363211197e5ee
Zachary Morel <morelz133@gmail.com>
2018-05-17 13:42:10 -0400

    Added first 2 components of the Course

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class AboutPage extends Component {
+  render(){ 
+    return(
+      <div>
+        <h1>About</h1>
+        <p>THis application uses React, redux, React Router and a variety of other helpful libraries.</p>
+      </div>
+    )
+  }
+}
