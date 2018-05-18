2fc2131b2f5a53c973c5ba39e96bc20e59c36957
Zachary Morel <morelz133@gmail.com>
2018-05-18 10:56:23 -0400

    Added Header, Courses, CSS styles

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import App from './components/app'
-import HomePage from './components/home/HomePage'
 import AboutPage from './components/about/AboutPage'
+import App from './components/app'
+import CoursesPage from './components/course/CoursesPage';
+import HomePage from './components/home/HomePage'
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={HomePage}/>
     <Route path='about' component={AboutPage}/>
+    <Route path='courses' component={CoursesPage}/>
   </Route>
 )
