d136b1154bb0afd31b17af14d1a10e8bd1c44e57
Zachary Morel <morelz133@gmail.com>
2018-05-23 14:51:55 -0400

    Adding first thunks

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,23 +6,8 @@ import { createCourse } from '../../actions/courseActions'
 class CoursesPage extends Component {
   constructor(props, context) {
     super(props, context)
-    this.state = {
-      course: { title: '' }
-    }
-
-    this.onTitleChange = this.onTitleChange.bind(this)
-    this.onClickSave = this.onClickSave.bind(this)
   }
 
-  onTitleChange(e) {
-    const course = this.state.course 
-    course.title = e.target.value
-    this.setState({ course: course })
-  }
-
-  onClickSave() {
-    this.props.createCourse(this.state.course)
-  }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>
@@ -32,18 +17,7 @@ class CoursesPage extends Component {
     return (
       <div>
         <h1>Courses</h1>
-        <h2>Add Course</h2>
         {this.props.courses.map(this.courseRow)}
-        <input
-          type='text'
-          onChange={this.onTitleChange}
-          value={this.state.course.title} 
-        />
-        <input 
-          type='submit'
-          value='Save'
-          onClick={this.onClickSave}
-        />
       </div>
     )
   }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 
 // initialState can be a nice place for rehydrating the application with server side data
@@ -7,6 +8,6 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer, 
     initialState, 
-    applyMiddleware(reduxImmutableStateInvariant())
+    applyMiddleware(thunk, reduxImmutableStateInvariant())
   )
 }
