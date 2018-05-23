e1f5df9abd452afa99fd98968fdccaaca6c22bca
Zachary Morel <morelz133@gmail.com>
2018-05-23 10:26:16 -0400

    Added Redux Store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
 import routes from './routes'
+import configureStore from './store/configureStore'
 import './styles/styles.css' // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
+// initialState can be a nice place for rehydrating the application with server side data
+// const store = configureStore(initialState)
+const store = configureStore() 
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 )
